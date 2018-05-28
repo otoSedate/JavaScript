0801b2db6a081559b1b3a4716ba81cbf3453a3c8
otoSedate <36985804+otoSedate@users.noreply.github.com>
2018-05-29 01:29:23 +0300

    php?

---
+++ _less2_1.php
@@ -0,0 +1,31 @@
+﻿<html>
+ <head>
+  <title>Тестируем PHP</title>
+ </head>
+ <body>
+<?php
+    $arr = ['a', 'b', 'c'];
+    var_dump($arr)
+?>
+ </body>
+  <body>
+<?php
+	$arr = ['a', 'b', 'c', 'd'];
+	echo $arr[0].'+'. $arr[1].', '.$arr[2].'+'.$arr[3]; 
+?>
+ </body>
+<body>
+   <?php
+	$num = '12345';
+	echo $num[0] + $num[1] + $num[2] + $num[3] + $num[4];
+?>
+</body>
+<body>
+$var = 10;
+$var++;
+$var++;
+$var--;
+echo $var;
+</body>
+
+</html>
