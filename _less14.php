e3f9bac54613202a5d2bbb43544913a522711ff3
otoSedate <36985804+otoSedate@users.noreply.github.com>
2018-05-31 17:26:16 +0300

    5455685

---
+++ _less14.php
@@ -0,0 +1,26 @@
+<html>
+ </head> 
+   <meta charset="utf-8">
+  <title>PHP</title>
+ </head>
+ <body>
+   <?php
+
+	function func($a, $b)
+ 	{
+		return $a == $b;
+	}
+	
+
+	function func1($num) 
+{
+	return $num < 0;
+}
+
+function func($a, $b)
+ {
+	return $a + $b > 10;
+}
+?>
+ </body>
+</html>
