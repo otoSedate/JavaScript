7c34f4c5d2cdcab85aab60f2fa39b17beff9bef9
otoSedate <36985804+otoSedate@users.noreply.github.com>
2018-05-31 16:53:12 +0300

    !!

---
+++ _less11_1.php
@@ -0,0 +1,39 @@
+﻿<html>
+ </head> 
+   <meta charset="utf-8">
+  <title>PHP</title>
+ </head>
+ <body>
+  <?php
+
+echo mktime(0, 0, 0, 3, 1, 2025);
+
+echo time() - mktime(13, 12, 59, 3, 15, 2000);
+
+echo date('Y.m.d H:i:s');
+
+$month = [
+		1=>'янв', 'фев', 'мар', 'апр', 'май', 'июнь',
+		'июль', 'авг', 'сен', 'окт', 'ноя', 'дек'
+	];
+	echo $month[date('n')];
+
+<form action="" method="GET">
+	<input type="text" name="date">
+	<input type="submit">
+</form>
+
+<?php
+	if (isset($_REQUEST['date'])) {
+		$date = explode('-', $_REQUEST['date']);
+		$month = [
+			1=>'янв', 'фев', 'мар', 'апр', 'май', 'июнь',
+			'июль', 'авг', 'сен', 'окт', 'ноя', 'дек'
+		];
+		echo $month[date('n',  mktime(0, 0, 0, $date[1], $date[2], $date[0]))];
+	}
+?>
+
+?>
+ </body>
+</html>
