03290b5e33e6be89960fab882ad04040ad481fac
otoSedate <36985804+otoSedate@users.noreply.github.com>
2018-05-31 17:04:51 +0300

    $$$

---
+++ _less12.php
@@ -0,0 +1,29 @@
+<html>
+ </head> 
+   <meta charset="utf-8">
+  <title>PHP</title>
+ </head>
+ <body>
+   <?php
+
+function func($elem){return $elem*$elem;}
+
+function func1($a, $b, $c)
+ {
+	return ($a - $b) / $c;
+}
+
+function func($num)
+ 	{
+		return $num * $num;
+	}
+
+function func($day)
+ {
+	$week = [1=>'пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+	return $arr[$week];
+}
+
+?>
+ </body>
+</html>
